cf850d5c70b2ac8ef8cb5ca56a180de55f3361e4
Yogeshwari <raoyogeshwari81@gmail.com>
2023-09-13 11:25:51 +0530

    Second commit

--- routes/fetcherLink.js
+++ routes/fetcherLink.js
@@ -2,7 +2,15 @@ var express = require("express");
 var blogTable = require("../model/table")
 
 var router = express.Router()
+
+const invalidChars = (body) => {
+    const {title} = body;
+    // 'Miss-'.split('').filter(v=>!/[a-z-]/.test(v.toLowerCase()))
+    return title?.split('').filter(v=>!/[a-z-]/.test(v.toLowerCase()))
+}
+
 router.post("/addInfo",(req,res) => {console.log("API Working",req.body)
+// if(invalidChars({...res.body}).length > 0){ return res.status(400).json({saveData:"Validation Failed!"})}
 var blogs = new blogTable({
     title: req.body.title,
     blogDate:req.body.blogblogdate,
@@ -38,21 +46,30 @@ router.post("/update/:id",(req,res)=> {
     }).catch((error) => {
         console.log("Error data", error);
         return res.status(400).json({ putData: error });
-    })
+    }) 
 })
 
-router.get("/getData/:title", (req, res) => {
-
-    console.log("paramsData",req.params.title)
-    blogTable
-    .findById({_id : req.params.title},{$set:req.body},{new: true})
-    // . get(_id, (req, res) => { const id = req.params.title; })
-    .then((data) => {
-      return res.status(200).json({ getData: data });
-    })
-    .catch((error) => {
-      console.log("Error data", error);
-      return res.status(400).json({ getData: error });
+router.get("/getData", (request, response) => {
+     if (request.query.search) {
+      // finding
+      console.log("Finding for: ", request.query.search);
+      Blog.findOne({ title: request.query.search })
+       .then((data) => {
+        return response.status(200).json({ data });
+       })
+       .catch((error) => {
+        return response.status(400).json({ data: error });
+       });
+     }
+     else {
+      Blog.find()
+       .then((data) => {
+        return response.status(200).json({ data });
+       })
+       .catch((error) => {
+        return response.status(400).json({ data: error });
+       });
+     }
     });
-});
+
 module.exports = router;
